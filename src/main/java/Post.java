96e77ae1171ff96416babef98c2d3997df78e6b8
Natalya Denisenko <ionova.natalya@mail.ru>
2024-11-09 17:13:01 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;
+    public String surname;
+    public String patronymic;
+    public String passport;
+    public String phone;
+    public FormData birthday = new FormData();
+    public boolean subscription;
+}
